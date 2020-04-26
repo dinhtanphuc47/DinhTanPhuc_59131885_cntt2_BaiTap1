a07b8e6792edd8062b1e1b0c7dac71aced6ed35d
dinhtanphuc47 <62933963+dinhtanphuc47@users.noreply.github.com>
2020-04-26 23:52:31 +0700

    first commit

---
+++ baitap1/Models/CalModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baitap1.Models
+{
+    public class CalModels
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public string pt { get; set; }
+    }
+}
